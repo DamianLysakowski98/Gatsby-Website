7c74ffa42102eca6fbb6b4d32fcaa38d1d94807d
DamianLysakowski <damianlysakowskifront98@gmail.com>
2022-01-27 22:23:39 +0100

    testv4 button:hover

--- src/components/Element.js/Form.js
+++ src/components/Element.js/Form.js
@@ -64,6 +64,11 @@ const StyledSubmit = styled.input`
   white-space: nowrap;
   margin: 0px;
   line-height: normal;
+  :hover {
+    color: black;
+    background-color: #fff;
+    border-color: black;
+  }
 `
 const StyledSelect = styled.select`
   border: 1px solid rgb(228, 229, 232);
